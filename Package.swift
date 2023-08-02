5475d5b469e6bdfcd52910dc0a305a6ff9314c52
Vonage GH Actions <github-actions@vonage.com>
2023-08-02 17:12:43 +0000

    release:v1.1.1-alpha.3

---
+++ Package.swift
@@ -0,0 +1,93 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "VonageClientSDKPackage",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "VonageClientSDKVoice",
+            targets: [
+                "ExternalDependencies",
+                "VonageWebRTC",
+                "NXMCore",
+                "VonageClientSDKCore",
+                "VonageClientSDKVoice"
+            ]
+        ),
+        .library(
+            name: "VonageClientSDKChat",
+            targets: [
+                "NXMCore",
+                "VonageClientSDKCore",
+                "VonageClientSDKChat"
+            ]
+        ),
+        .library(
+            name: "VonageClientSDK",
+            targets: [
+                "ExternalDependencies",
+                "VonageWebRTC",
+                "NXMCore",
+                "VonageClientSDKCore",
+                "VonageClientSDKVoice",
+                "VonageClientSDKChat",
+                "VonageClientSDK"
+            ]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        // External Dependencies
+        .target(
+            name: "ExternalDependencies",
+            path: "./Dummy",
+            linkerSettings: [
+                .linkedLibrary("c++"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("GLKit"),
+                .linkedFramework("VideoToolbox"),
+                .linkedFramework("CoreAudio"),
+                .linkedFramework("Network"),
+                .linkedFramework("MetalKit")
+            ]
+        ),
+        // VonageWebRTC
+        .binaryTarget(
+            name: "VonageWebRTC",
+            url: "https://d3opqjmqzxf057.cloudfront.net/vonage-webrtc/pod/vonagewebrtc/release/99.2.39/VonageWebRTC-99.2.39.zip",
+            checksum: "5b8912086bc55da7c938e672a473315bab707a5de6a499bc8a83555abc920ae4"
+        ),
+        // Internal Frameworks
+        .binaryTarget(
+            name: "NXMCore",
+            url: "https://cs-sdk.main0.api.rtc.prd.euw1.vonagenetworks.net/public/1.1.1-alpha.3/ios/SPM-NXMCore-1.1.1-alpha.3-Release.zip",
+            checksum: "31b291cfbfe798231d8c285997709620e645a5862df7fb722f97ac672b83eb11"
+        ),
+        .binaryTarget(
+            name: "VonageClientSDKCore",
+            url: "https://cs-sdk.main0.api.rtc.prd.euw1.vonagenetworks.net/public/1.1.1-alpha.3/ios/SPM-VonageClientSDKCore-1.1.1-alpha.3-Release.zip",
+            checksum: "9cb500e80ac2f488dea2224f7d07fce9352e175ad45c7c17bb8f3b2e9691ec1c"
+        ),
+        .binaryTarget(
+            name: "VonageClientSDKChat",
+            url: "https://cs-sdk.main0.api.rtc.prd.euw1.vonagenetworks.net/public/1.1.1-alpha.3/ios/SPM-VonageClientSDKChat-1.1.1-alpha.3-Release.zip",
+            checksum: "3d136cc563483dadbdbd0c1c287dd2e0ecdaa4d0a406594d0bcb805ff93f6e87"
+        ),
+        .binaryTarget(
+            name: "VonageClientSDKVoice",
+            url: "https://cs-sdk.main0.api.rtc.prd.euw1.vonagenetworks.net/public/1.1.1-alpha.3/ios/SPM-VonageClientSDKVoice-1.1.1-alpha.3-Release.zip",
+            checksum: "b000d41cb88d772698d3180168632721e1c315d562bfb7860ff69375fe06cce7"
+        ),
+        .binaryTarget(
+            name: "VonageClientSDK",
+            url: "https://cs-sdk.main0.api.rtc.prd.euw1.vonagenetworks.net/public/1.1.1-alpha.3/ios/SPM-VonageClientSDK-1.1.1-alpha.3-Release.zip",
+            checksum: "b8f17063c6a3181e5dfd431938f5c39b38525379ebc41658385742819e57f116"
+        ),
+    ]
+)
